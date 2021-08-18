12260f7f60ade344e53e55d312b5a1b99ed359e9
Ace Wu <acewu.tw@gmail.com>
2021-08-19 04:04:32 +0800

    add leetcode #91

--- leetcode/0091-decode-ways.py
+++ leetcode/0091-decode-ways.py
@@ -1,12 +1,25 @@
-mapping = {int(i): c for i, c in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1)}
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        codes = set(str(i) for i in range(1, 27))
+        if s[0] not in codes:
+            return 0
+        c2, c1, c = 1, 1, 0
+        for i in range(1, len(s)):
+            if s[i:i+1] in codes:
+                c += c1
+            if s[i-1:i+1] in codes:
+                c += c2
+            c2, c1, c = c1, c, 0
+        return c1
 
-def num_decodings(text):
-    p1, p2 = 1, 0
-    for i in range(len(text) - 1, -1, -1):
-        print(text[i])
 
-class Solution(object):
-    def numDecodings(self, s):
-        pass
+## TC: O(n)
+## SC: O(1)
 
-num_decodings('test')
+s = Solution()
+print(s.numDecodings('12'))
+print(s.numDecodings('27'))
+print(s.numDecodings('226'))
+print(s.numDecodings('22222'))
+print(s.numDecodings('0123'))
+
