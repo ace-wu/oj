8cb1539e63eefe7a63ec4ee779505a10dc0e85ae
Ace Wu <acewu.tw@gmail.com>
2021-10-09 23:53:32 +0800

    add leetcode #53

---
+++ leetcode/0053-maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            if nums[i-1]> 0:
+                nums[i] += nums[i-1]
+        return max(nums)
+
+
+## TC: O(n)
+## SC: O(1)
