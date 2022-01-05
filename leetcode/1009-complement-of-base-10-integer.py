605d5ac33b7b19c9febd97132cf859599e743ea5
Ace Wu <acewu.tw@gmail.com>
2022-01-05 23:14:58 +0800

    fix leetcode #1009

--- leetcode/1009-complement-of-base-10-integer.py
+++ leetcode/1009-complement-of-base-10-integer.py
@@ -2,6 +2,8 @@ class Solution:
     ## TC: O(log(n))
     ## SC: O(1)
     def bitwiseComplement(self, n: int) -> int:
+        if n == 0:
+            return 1
         bit = 1
         while bit <= n:
             bit <<= 1
