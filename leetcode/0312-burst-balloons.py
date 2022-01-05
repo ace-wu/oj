d78afc18d54534dd195c699b4c6140680802dfd9
Ace Wu <acewu.tw@gmail.com>
2022-01-06 02:34:40 +0800

    refine leetcode #312

--- leetcode/0312-burst-balloons.py
+++ leetcode/0312-burst-balloons.py
@@ -1,28 +1,34 @@
-# https://leetcode.com/problems/burst-balloons/
-
-def max_coins(nums):
-    if not nums:
-        return 0
-    nums = [1] + nums + [1]
-    size = len(nums)
-    table = [[0] * size for i in range(size)]
-
-    for w in range(2, size):
-        for i in range(size - w):
-            j = i + w
-            prod_ij = nums[i] * nums[j]
-            table[i][j] = max([nums[k] * prod_ij + table[i][k] + table[k][j] for k in range(i + 1, j)])
-
-    return table[0][size-1]
+from functools import cache
 
 
-class Solution(object):
-    def maxCoins(self, nums):
-        return max_coins(nums)
+class Solution:
+    # top-down DP (TLE)
+    ## TC: O(n^2)
+    ## SC: O(n^2)
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        @cache
+        def max_coins(left, right):
+            if left + 1 >= right:
+                return 0
+            left_right_prod = nums[left] * nums[right]
+            return max(nums[mid] * left_right_prod + max_coins(left, mid) + max_coins(mid, right) for mid in range(left + 1, right))
+        return max_coins(0, len(nums) - 1)
 
+    # bottom-up DP
+    ## TC: O(n^2)
+    ## SC: O(n^2)
+    def maxCoins(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        nums = [1] + nums + [1]
+        size = len(nums)
+        table = [[0] * size for i in range(size)]
 
-#print(max_coins([2]))
-#print(max_coins([2, 1, 2]))
-print(max_coins([3, 1, 5, 8]))
+        for width in range(2, size):
+            for left in range(size - width):
+                right = left + width
+                left_right_prod = nums[left] * nums[right]
+                table[left][right] = max([nums[mid] * left_right_prod + table[left][mid] + table[mid][right] for mid in range(left + 1, right)])
 
-
+        return table[0][size-1]
