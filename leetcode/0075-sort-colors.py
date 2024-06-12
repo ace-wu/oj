f9a3f977d739f54e3d2be479a14e5fab41d6f4a0
Ace Wu <acewu.tw@gmail.com>
2024-06-12 21:45:59 +0800

    update leetcode #75

--- leetcode/0075-sort-colors.py
+++ leetcode/0075-sort-colors.py
@@ -1,8 +1,10 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    ## TC: O(n)
+    ## SC: in-place
+    def sortColors_swap(self, nums: list[int]) -> None:
         i, left, right = 0, 0, len(nums) - 1
         while i <= right:
-            if nums[i] == 0 :
+            if nums[i] == 0:
                 nums[left], nums[i] = nums[i], nums[left]
                 left += 1
                 i += 1
@@ -12,6 +14,18 @@ class Solution:
             else:
                 i += 1
 
-
-## TC: O(n)
-## SC: O(1)
+    ## TC: O(n)
+    ## SC: in-place
+    def sortColors_counting(self, nums: list[int]) -> None:
+        zeros, ones, n = 0, 0, len(nums)
+        for i in nums:
+            if i == 0:
+                zeros += 1
+            elif i == 1:
+                ones += 1
+        for i in range(zeros):
+            nums[i] = 0
+        for i in range(zeros, zeros + ones):
+            nums[i] = 1
+        for i in range(zeros + ones, n):
+            nums[i] = 2
